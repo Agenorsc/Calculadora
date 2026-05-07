20452dbebd6baa4fa77515f152c0ad1c3e2e0cef
Agenorsc <agenorsc@hotmail.com>
2026-05-07 19:00:12 -0300

    first commit

---
+++ CalculadoraTest.java
@@ -0,0 +1,34 @@
+package com.exemplo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CalculadoraTest {
+
+    Calculadora calc = new Calculadora();
+
+    // Teste Individual
+    @Test
+    void testeSomaSimples() {
+        assertEquals(10, calc.somar(7, 3), "7 + 3 deve ser 10");
+    }
+
+    @Test
+    void testeDivisaoPorZero() {
+        assertThrows(ArithmeticException.class, () -> calc.dividir(10, 0));
+    }
+
+    // Teste em Grupo (Validação em Massa)
+    @ParameterizedTest
+    @CsvSource({
+        "10, 5, 2",
+        "20, 4, 5",
+        "100, 10, 10",
+        "9, 3, 3"
+    })
+    void testeDivisoesEmGrupo(double a, double b, double resultadoEsperado) {
+        assertEquals(resultadoEsperado, calc.dividir(a, b));
+    }
+}
